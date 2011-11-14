1a3067142ab268e8adf43a7bb3a54f8b96330e32
Dmitry Ketov <dketov@gmail.com>
2011-11-14 11:23:46 +0400

    English language localization

--- lang/en/auth_loginza.php
+++ lang/en/auth_loginza.php
@@ -1,1 +1,10 @@
-../ru/auth_loginza.php
+<?php
+
+$string['pluginname'] = 'Authentication broker loginza.ru';
+$string['auth_loginzadescription'] = 'Login by using facebook/google/flickr/openid accounts through the <a href="http://loginza.ru/?lang=en">loginza.ru</a> authentication broker.<br/>To improve security while logging in, be sure to use <strong>secure token verification</strong> (by using ID and secret key) in the settings of your Loginza account. You can acquire a widget ID and secret key in the "My Loginza widget" section of your Loginza account.';
+
+$string['auth_loginzabutton'] = 'Login through the loginza.ru';
+$string['id'] = 'ID:';
+$string['skey'] = 'Secret key:';
+
+?>
