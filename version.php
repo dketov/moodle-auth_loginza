bbc5c897d3cc762752e0e05b42f32a0cff344936
Dmitry Ketov <dketov@gmail.com>
2011-11-14 11:26:31 +0400

    copy-and-paste fix

--- version.php
+++ version.php
@@ -3,6 +3,6 @@
 $plugin->version = 2011111300;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.0.1';
-$module->requires  = 201107010; // Was (honestly) tested on 2011070102.02
+$plugin->requires  = 201107010; // Was (honestly) tested on 2011070102.02
 
 ?>
