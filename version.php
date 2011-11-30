50df619509bf38630bd7fb6565f46bba9846a356
Dmitry Ketov <dketov@gmail.com>
2011-12-01 01:28:40 +0400

    Version change to publish at Moodle Plugins Directory

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2011112800;
+$plugin->version = 2011120100;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.0.1';
 $plugin->requires  = 2011070100; // Was (honestly) tested on 2011070102.02
